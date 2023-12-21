d3984e7208fafd642f75e356cb077e3ddfa35e1f
JorgeHCamara <jorgehcamara@hotmail.com>
2023-12-21 16:00:46 -0300

    chore: book service added

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LivroService } from 'src/app/services/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   listaLivros: [];
+  searchField: string = '';
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private service: LivroService) { }
 
+  searchBooks() {
+    this.subscription = this.service.searchBooksApi(this.searchField).subscribe({
+      next: apiReturn => console.log(apiReturn),
+      error: error => console.error(error),
+      complete: () => console.log('Observable completado')
+    }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
 
 
